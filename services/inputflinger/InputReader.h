0b2a127feddeae104ebf82be1ada67853dfac0d6
David Müller <david.mueller1013@gmail.com>
2017-08-27 23:59:58 +0200

    fix order
    frameworks/native/services/inputflinger/InputReader.h:253:13: error: field 'stylusPalmRejectionTime' will be initialized after field 'showTouches' [-Werror,-Wreorder]
                stylusPalmRejectionTime(50 * 10000000LL), // 50 ms

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -228,11 +228,11 @@ struct InputReaderConfiguration {
     // will cover this portion of the display diagonal.
     float pointerGestureZoomSpeedRatio;
 
-    // True to show the location of touches on the touch screen as spots.
-    bool showTouches;
-    
     // Ignore finger touches this long after the stylus has been used (including hover)
     nsecs_t stylusPalmRejectionTime;
+
+    // True to show the location of touches on the touch screen as spots.
+    bool showTouches;
 
     InputReaderConfiguration() :
             virtualKeyQuietTime(0),
